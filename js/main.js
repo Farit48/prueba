8621c71f37504c8059a94b0bc57c8fd0f7da382e
Farit48 <faritcadena00@gmail.com>
2025-04-24 04:51:37 -0500

    prii

---
+++ js/main.js
@@ -0,0 +1,135 @@
+let db;
+function abrirDB(callback){
+    const request = indexedDB.open("My DataBase", 1);
+    
+    request.onerror = (e)=>{
+        console.log("Error al Leer la base de Datos", e.target.error);
+    };
+
+    request.onsuccess = (e)=>{
+        db = e.target.result
+        callback(db);
+        console.log("Base de datos abierta")
+        
+    }
+    request.onupgradeneeded = (e)=>{
+        const db = e.target.result
+        if(!db.objectStoreNames.contains("Usuarios")){
+            db.createObjectStore("Usuarios", {keyPath: "id"}).createIndex("porCorreo", "correo", {unique:true})
+            console.log("Se ha actualizado la base de datos Con exito", db)
+        }
+    } 
+}
+
+class Mimain extends HTMLElement {
+    constructor(){
+        super();
+        this.innerHTML = `
+        <main class="main-etiqueta">
+            <section class="main_header">
+                <h1>Bienvenido a Acme School</h1>
+            </section>
+            <section class="main_form">
+                <form action="" id="frmGetIn" class="main_form-elements">
+                    <p>Inicia sesion con el correo</p>
+                    <input type="text" name="Usuario" id="email">
+                    <p>Ingrese contraseña</p>
+                    <input type="password" name="password" id="password"><br>
+                    <button type="button" id="btnGetin">Iniciar</button>
+                    <br>
+                    <button type="button" id="btnRE">Registrar</button>
+                </form> 
+            </section>
+        </main>
+      
+          
+        `
+    }
+   
+
+    connectedCallback(){
+        console.log("ya me conecte al doom")
+        const btnRE = document.getElementById('btnRE')
+        btnRE.addEventListener('click',()=>{
+            
+            
+            const main = document.getElementById('main')
+            main.setAttribute('vista', 'registro')
+            main.innerHTML=`<mi-dash><mi-dash>`
+            
+            
+        })
+        const btnGetin = document.getElementById('btnGetin')
+        btnGetin.addEventListener('click', ()=>{
+            
+            
+            abrirDB((db)=>{
+                const correo = document.getElementById('email').value;
+                const pass = document.getElementById('password').value;
+        
+                const transaction = db.transaction(["Usuarios"], "readonly");
+                const store = transaction.objectStore("Usuarios");
+                const index = store.index("porCorreo");
+        
+                const request = index.get(correo);
+        
+                request.onsuccess = () => {
+                    const usuario = request.result;
+                    if (usuario) {
+                        if (usuario.contraseña === pass && usuario.correo === correo) {
+                            main.innerHTML=`
+                                <style>
+                                .efecto{
+                                    opacity:0;
+                                    animation: animation 4s ease 0.1s forwards ;
+                                    width:50%;
+                                    margin:auto;
+                                }
+                                @keyframes animation {
+                                    0%{opacity: 0;}
+                                    50%{opacity: 1;}
+                                    75%{opacity: 0;}
+                                    100%{opacity: 1;}
+                                }
+                                </style>
+                                <h1 class="efecto">Iniciando Sesion</h1>`
+                            setTimeout(() => {
+                                window.location.href= "dashboard.html"
+                            }, 5000);
+                        } else {
+                            main.innerHTML=`
+                                <style>
+                                .efecto{
+                                    opacity:0;
+                                    animation: animation 4s ease 0.1s forwards ;
+                                    width:50%;
+                                    margin:auto;
+                                }
+                                @keyframes animation {
+                                    0%{opacity: 0;}
+                                    50%{opacity: 1;}
+                                    75%{opacity: 0;}
+                                    100%{opacity: 1;}
+                                }
+                                </style>
+                                <h1 class="efecto">Usuario invalidao</h1>`
+                            setTimeout(() => {
+                                window.location.href= "index.html"
+                            }, 2000);
+                        }   
+                    } else {
+                        console.log('Usuario no registrado');
+                    }
+                }; 
+            })
+        })
+        
+       
+        
+        
+    }
+
+  
+}
+customElements.define("mi-main", Mimain )
+
---
+++ js/registro.js
@@ -0,0 +1,89 @@
+class Registro extends HTMLElement {
+    constructor(){
+        super();
+        this.innerHTML = `
+        <main class="main-etiqueta">
+            <section class="main_header">
+                <h2>Registrar</h2>
+            </section>
+            <section class="main_form">
+                <form action="" id="frmRegist" class="main_form-elements">
+                    <p>Correo nuevo</p>
+                    <input type="text" name="Usuario" id="emailReg">
+                    <p>Ingrese contraseña nueva</p>
+                    <input type="password" name="password" id="passwordReg">
+                    <p>id nuevo</p>
+                    <input type="text" name="id" id="id"><br>
+                    <button type="button" id="btnSave">guardar</button>
+                </form>
+            </section>
+            
+        </main>
+        `
+    }
+    connectedCallback(){
+        console.log('hola soy la nueva etiqueda conectada aldoom')
+        const btnSave = document.getElementById('btnSave')
+        btnSave.addEventListener('click', ()=>{
+            abrirDB((db)=>{
+                console.log('Se dio click en guardar')
+                const nuevoUser = {
+                id:document.getElementById('id').value,
+                correo:document.getElementById('emailReg').value,
+                contraseña:document.getElementById('passwordReg').value
+                }
+            
+                const transaction = db.transaction(["Usuarios"], "readwrite")
+                const mibase = transaction.objectStore("Usuarios")
+                const addmibase = mibase.add(nuevoUser)
+                addmibase.onsuccess = ()=>{
+                    console.log('Se agrego correctamente al object store')
+                    const main = document.getElementById('main')
+                    main.innerHTML=`
+                    <style>
+                    .efecto{
+                        opacity:0;
+                        animation: animation 4s ease 0.1s forwards ;
+                        width:50%;
+                        margin:auto;
+                    }
+                    @keyframes animation {
+                        0%{opacity: 0;}
+                        50%{opacity: 1;}
+                        75%{opacity: 0;}
+                        100%{opacity: 1;}
+                    }
+                    </style>
+                    <h1 class="efecto">AGREGANDO EL USUARIO</h1>`
+                    setTimeout(()=>{
+                        main.innerHTML=`
+                            <style>
+                            .efecto{
+                                opacity:0;
+                                animation: animation 4s ease 0.1s forwards ;
+                                width:50%;
+                                margin:auto;
+                            }
+                            @keyframes animation {
+                                0%{opacity: 0;}
+                                50%{opacity: 1;}
+                                75%{opacity: 0;}
+                                100%{opacity: 1;}
+                            }
+                            </style>
+                            <h1 class="efecto">USUARIO AGREGADO CON EXITO</h1>`
+                    }, 5000)
+                    setTimeout(()=>{
+                        window.location.href = "/index.html"
+                    }, 10000)
+                    
+                    
+                }
+                addmibase.onerror = ()=>{
+                    console.log("Erro al agregar")
+                }
+            })
+        })
+    }
+}
+customElements.define('mi-dash', Registro )
